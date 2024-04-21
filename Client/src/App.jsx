21e1fc4caa3e02d18d770d13d863ed3232509e4f
Ashish poudel <techcode057@gmail.com>
2024-04-21 14:16:35 +0545

    header

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../src/pages/Home';
-import About from "../src/pages/About"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../src/pages/Home";
+import About from "../src/pages/About";
+import Profile from "../src/pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignOut from "./pages/SignOut";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    </Routes>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/signOut" element={<SignOut />} />
+      </Routes>
     </BrowserRouter>
   );
 }
